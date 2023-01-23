fixes to averaging and rms calculation

build/lib.linux-x86_64-3.8/uio_tools/uio_reader.py
@@ -191,7 +191,7 @@ class UIOData():
         'ke': lambda: calculate_kinetic_energy(self.rho, self.v1, self.v2, self.v3),
         'momentum': lambda: calculate_momentum(self.rho, self.v1, self.v2, self.v3),
         # Mean-computed quantities (requires mean file!)
-        'v3rms': lambda: np.sqrt(np.abs(spatial_mean(self['v3'])**2) - spatial_mean(self['v3']**2))
+        'v3rms': lambda: np.sqrt(np.abs(spatial_mean(self['v3'])**2 - spatial_mean(self['v3']**2)))
     }
 
     self.eos_dict = {

uio_tools/uio_utils.py
@@ -24,9 +24,9 @@ def spatial_mean(arr: np.ndarray, out_axis=0):
   # Average over 2 spatial axes, 'out_axis' is the axis NOT averaged over
   # e.g. if you want to average over axes (1, 2) (conventionally averaging
   # across (x, y)), pass in 'out_axis = 0'
-  axes = [0, 1, 2]
-  axis = axes.pop(out_axis)
-  return np.mean(arr, axis=axis)
+  axis = [0, 1, 2]
+  axis.pop(out_axis)
+  return np.mean(arr, axis=tuple(axis))
 
 
 # -------------------------------------------------------------------------